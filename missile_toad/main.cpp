b270f81236e85dd474c59652821f620b2e1292d4
Alan Ramírez Herrera <13205973+Alan5142@users.noreply.github.com>
2023-08-21 17:09:48 -0600

    feat: Added switch support (#6)
    * feat: Added switch support
    
    * ci: changed switch build name
    
    * ci: fix switch directory pipeline
    
    * ci: fix switch directory pipeline
    
    * ci: fix switch directory pipeline
    
    * fix: formatted code
    ci: replaced build type with debug

--- missile_toad/main.cpp
+++ missile_toad/main.cpp
@@ -1,29 +1,68 @@
 #include "raylib-cpp.hpp"
+#include "raylib.h"
+#include <filesystem>
+#include <fmt/format.h>
+#include <gsl/gsl>
+#include <iostream>
+#include <physfs.h>
+
+#ifdef PLATFORM_NX
+#    include <switch.h>
+#endif
 
 #ifdef WIN32
 #    pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 #endif
 
-int main()
+unsigned char *load_file_data_callback(const char *fileName, unsigned int *bytesRead)
 {
-    int            screenWidth  = 800;
-    int            screenHeight = 450;
+    PHYSFS_File *file = PHYSFS_openRead(fileName);
+    if (file == nullptr)
+    {
+        return nullptr;
+    }
+    PHYSFS_sint64 size = PHYSFS_fileLength(file);
+    auto         *data = gsl::owner<unsigned char *>(new unsigned char[size]);
+    PHYSFS_readBytes(file, data, size);
+    PHYSFS_close(file);
+    *bytesRead = size;
+    return data;
+}
+
+int main(int argc, char *argv[])
+{
+#ifdef PLATFORM_NX
+    romfsInit();
+#endif
+    int screenWidth  = 1280;
+    int screenHeight = 720;
+
+    // NO-LINT
+    if (PHYSFS_init(argv[0]) == 0)
+    {
+        return 1;
+    }
+
+    ::SetLoadFileDataCallback(load_file_data_callback);
+
+    PHYSFS_mount("romfs:/", "/", 1);
     raylib::Window window(screenWidth, screenHeight, "raylib-cpp - basic window");
 
-    SetTargetFPS(60);
+    raylib::Texture2D texture("/resources/raylib_logo.png");
+
+    raylib::Camera2D camera({0, 0}, {0, 0}, 0, 1);
 
     while (!window.ShouldClose())
     {
-        BeginDrawing();
-
         window.ClearBackground(RAYWHITE);
-
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
         EndDrawing();
     }
 
     // UnloadTexture() and CloseWindow() are called automatically.
+
+#ifdef PLATFORM_NX
+    romfsExit();
+#endif
 
     return 0;
 }
