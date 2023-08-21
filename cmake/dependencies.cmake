36c512903b8a318bceaee767d5a664e855d65317
Alan Ramirez Herrera <alan5142@hotmail.com>
2023-08-21 16:50:53 -0600

    feat: Added switch support

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,11 +1,8 @@
 # Raylib
-CPMAddPackage("https://github.com/raysan5/raylib.git#4.5.0")
+CPMAddPackage("https://github.com/Alan5142/raylib-nx.git#9b47785")
 
 # Raylib cpp
-CPMAddPackage("https://github.com/RobLoach/raylib-cpp.git@4.5.1")
-
-# spdlog
-CPMAddPackage("https://github.com/gabime/spdlog.git@1.12.0")
+CPMAddPackage("https://github.com/RobLoach/raylib-cpp.git#bf0be19")
 
 # Catch2
 CPMAddPackage("https://github.com/catchorg/Catch2.git@3.4.0")
@@ -13,9 +10,17 @@ CPMAddPackage("https://github.com/catchorg/Catch2.git@3.4.0")
 # fmt
 CPMAddPackage("https://github.com/fmtlib/fmt.git#10.1.0")
 
-# Disable all warnings for raylib and raylib-cpp
+# PhysFS
+CPMAddPackage("https://github.com/Alan5142/physfs-nx.git#switch")
+
+# GSL
+CPMAddPackage("https://github.com/microsoft/GSL.git#4300304")
+
+# Disable all warnings for raylib and physfs
 if (MSVC)
     target_compile_options(raylib PRIVATE /W0)
+    target_compile_options(physfs-static PRIVATE /W0)
 else ()
     target_compile_options(raylib PRIVATE -w)
+    target_compile_options(physfs-static PRIVATE -w)
 endif ()
