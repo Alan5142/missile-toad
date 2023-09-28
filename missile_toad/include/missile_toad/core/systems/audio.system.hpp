ae2be25802f5fda0c46ea87897ad7c0bd3d38717
Saúl Napoleón Haro Hernández <saulnharoh@gmail.com>
2023-09-28 07:30:19 -0600

    Audio system update

--- missile_toad/include/missile_toad/core/systems/audio.system.hpp
+++ missile_toad/include/missile_toad/core/systems/audio.system.hpp
@@ -2,19 +2,29 @@
 #include "missile_toad/core/base_system.hpp"
 #include "missile_toad/core/common.hpp"
 
+#include <entt/entity/registry.hpp>
 #include <entt/meta/meta.hpp>
 
 namespace missiletoad::core
 {
     class Game;
-}
 
-namespace missiletoad::core
-{
     class AudioSystem : public missiletoad::core::BaseSystem
     {
+        entt::registry *registry_;
+
     public:
-        AudioSystem(missiletoad::core::Game *game);
+        AudioSystem(Game *game);
+        AudioSystem(const AudioSystem &) = delete;
+        AudioSystem(AudioSystem &&)      = delete;
+
+        AudioSystem &operator=(const AudioSystem &) = delete;
+        AudioSystem &operator=(AudioSystem &&)      = delete;
+
+        ~AudioSystem() override;
+
         static void register_system(entt::meta_ctx &ctx);
-    };
+
+        void on_update(float delta_time) override;
+    }; // namespace missiletoad::core
 } // namespace missiletoad::core
--- missile_toad/src/core/systems/audio.system.cpp
+++ missile_toad/src/core/systems/audio.system.cpp
@@ -1,14 +1,15 @@
+
 #include "missile_toad/core/systems/audio.system.hpp"
 #include "missile_toad/core/components/audio.component.hpp"
 #include "missile_toad/core/game.hpp"
 
+#include <entt/entity/registry.hpp>
 #include <entt/meta/factory.hpp>
 #include <entt/meta/meta.hpp>
 
 missiletoad::core::AudioSystem::AudioSystem(missiletoad::core::Game *game)
+    : registry_(&game->active_scene().get_registry())
 {
-    // TODO: Add your constructor code here
-    unused(game);
 }
 
 void missiletoad::core::AudioSystem::register_system(entt::meta_ctx &ctx)
@@ -19,4 +20,21 @@ void missiletoad::core::AudioSystem::register_system(entt::meta_ctx &ctx)
         .base<missiletoad::core::BaseSystem>()
         .ctor<missiletoad::core::Game *>();
     // TODO: Add your register code here
+}
+
+void missiletoad::core::AudioSystem::on_update(float delta_time)
+{
+    auto view = registry_->view<core::AudioComponent>();
+
+    unused(delta_time);
+
+    for (auto entity : view)
+    {
+        auto &audio = view.get<core::AudioComponent>(entity);
+        audio.sound->get_music().Update();
+    }
+}
+
+missiletoad::core::AudioSystem::~AudioSystem()
+{
 }
